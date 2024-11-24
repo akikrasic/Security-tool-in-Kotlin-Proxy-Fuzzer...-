b5bbd6179f64b266f4fc90f4d2aaee3da1b74256
Aleksandar Krasic Aki <aki@zmaj.akikrasic.srb>
2024-11-24 22:31:52 +0100

    Додата правилна висина и однос табеле и простора за приказ тела код слања захтева

--- src/main/kotlin/srb/akikrasic/forma/paneli/MojeSlanjeZahtevaPanel.kt
+++ src/main/kotlin/srb/akikrasic/forma/paneli/MojeSlanjeZahtevaPanel.kt
@@ -2,9 +2,11 @@ package srb.akikrasic.forma.paneli
 
 import srb.akikrasic.forma.modelitabele.ModelTabeleHederi
 import srb.akikrasic.korisno.GuiKorisno
+import srb.akikrasic.podaci.HederIVrednost
 import srb.akikrasic.ucitavanjezahtevaiodgovora.Zahtev
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
+import java.awt.HeadlessException
 import java.awt.Insets
 import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
@@ -43,12 +45,12 @@ class MojeSlanjeZahtevaPanel(val zahtev: Zahtev = Zahtev()) : JPanel(){
         listOf("GET", "POST", "PUT", "DELETE").forEach {
             metodaKombo.addItem(it)
         }
-        urlPolje.text = "https://"
+
         layout = GridBagLayout()
         val c = GuiKorisno.napraviteGridBagConstraints()
 
         c.weightx = 1.0
-        c.weighty =0.05
+        c.weighty =0.1
 
         val panelGore = napravitePanelGore()
         add(panelGore, c)
@@ -57,11 +59,11 @@ class MojeSlanjeZahtevaPanel(val zahtev: Zahtev = Zahtev()) : JPanel(){
         c.gridx = 0
         c.gridy = 1
         c.weightx = 1.0
-        c.weighty = 0.05
+        c.weighty = 0.45
         c.gridheight = 1
         add(JScrollPane(tabelaHederi), c)
         c.gridy = 2
-        c.weighty =0.9
+        c.weighty =0.45
         add(JScrollPane(zahtevTeloArea), c)
 
         c.gridx = 1
@@ -111,6 +113,16 @@ class MojeSlanjeZahtevaPanel(val zahtev: Zahtev = Zahtev()) : JPanel(){
             }
 
         })
+        if( zahtev.url !=""){
+            urlPolje.text = zahtev.url
+        }
+        else{
+            urlPolje.text = "https://"
+        }
+        modelTabeleHederi.lista.clear()
+        modelTabeleHederi.lista.addAll(zahtev.hederi.mapaOriginalnihHedera.map{HederIVrednost(it.key, it.value)})
+        modelTabeleHederi.lista.add(HederIVrednost())
+        zahtevTeloArea.text = String(zahtev.telo)
     }
 
     fun napravitePanelGore():JPanel{
