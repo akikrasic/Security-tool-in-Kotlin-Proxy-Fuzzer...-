aa3711ba2e7df73fa7867b01590b156f53895c5d
Aki Krasic <akikrasic@gmail.com>
2026-01-26 22:23:49 +0100

    Нешто сам чачкао ХедериНазиви

--- src/main/kotlin/srb/akikrasic/ucitavanjezahtevaiodgovora/konstante/HederiNazivi.kt
+++ src/main/kotlin/srb/akikrasic/ucitavanjezahtevaiodgovora/konstante/HederiNazivi.kt
@@ -2,7 +2,7 @@ package srb.akikrasic.ucitavanjezahtevaiodgovora.konstante
 
 object HederiNazivi {
     val transferEncoding = "Transfer-Encoding"
-    val transferEncodingBig = transferEncoding.toUpperCase()
+    val transferEncodingBig = transferEncoding.uppercase()
     val contentLength = "Content-Length"
     val contentEncoding="Content-Encoding"
     val host = "Host"
