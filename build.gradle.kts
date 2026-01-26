534f42976f91ff794aafe6fa3a9ff4e070a5bf04
Aki Krasic <akikrasic@gmail.com>
2026-01-26 22:22:33 +0100

    Додате библиотеке за испитивање како и зашто ради не ради слање пост методе

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.2.0"
 }
 
 group = "srb.aki"
@@ -17,38 +17,43 @@ repositories {
 
 dependencies {
     // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
-
-    implementation("org.bouncycastle:bcpkix-fips:1.0.7")
-    implementation("org.bouncycastle:bctls-fips:1.0.17")
+    //prckao sam ove biblioteke bc verzije i ovu commons compress a dirao sam i 33. liniju da vdimo sto ovo ovde radi a tamo ne radi
+    implementation("org.bouncycastle:bcpkix-fips:2.0.8")
+    implementation("org.bouncycastle:bctls-fips:1.0.19")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("com.nixxcode.jvmbrotli:jvmbrotli:0.2.0")
     implementation("com.nixxcode.jvmbrotli:jvmbrotli-linux-x86-amd64:0.2.0")
     implementation("com.nixxcode.jvmbrotli:jvmbrotli-parent:0.2.0")
-    implementation("org.apache.commons:commons-compress:1.27.1")
+    implementation("org.apache.commons:commons-compress:1.28.0")
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:5.3.0"))
+    // define any required OkHttp artifacts without version
+    implementation("com.squareup.okhttp3:okhttp")
 
 
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8")) //zbog ovo izgleda radi ovo
     testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
 
-tasks.test {
-    useJUnitPlatform()
+kotlin {
+    jvmToolchain(25)
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "21"
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
 }
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "21"
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_24) // Or your desired JVM version
+    }
 }
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "21"
+java {
+    targetCompatibility = JavaVersion.VERSION_24 // Or your desired JVM version
+    sourceCompatibility = JavaVersion.VERSION_24 // Or your desired JVM version
 }
-
 
 
